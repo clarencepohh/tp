009bbad833b5981b5b7654577f0de32446354607
NicholasTanYY <nicholastanyunyu@gmail.com>
2024-03-17 15:49:08 +0800

    Remove unnecessary comments

--- src/main/java/ui/AnsiColors.java
+++ src/main/java/ui/AnsiColors.java
@@ -1,10 +1,8 @@
 package ui;
 
 public class AnsiColors {
-    // Reset code
     public static final String RESET = "\u001B[0m";
 
-    // Regular Colors
     public static final String BLACK = "\u001B[30m";
     public static final String RED = "\u001B[31m";
     public static final String GREEN = "\u001B[32m";
@@ -14,7 +12,6 @@ public class AnsiColors {
     public static final String CYAN = "\u001B[36m";
     public static final String WHITE = "\u001B[37m";
 
-    // Bold
     public static final String BLACK_BOLD = "\u001B[1;30m";
     public static final String RED_BOLD = "\u001B[1;31m";
     public static final String GREEN_BOLD = "\u001B[1;32m";
@@ -24,7 +21,6 @@ public class AnsiColors {
     public static final String CYAN_BOLD = "\u001B[1;36m";
     public static final String WHITE_BOLD = "\u001B[1;37m";
 
-    // Underline
     public static final String BLACK_UNDERLINED = "\u001B[4;30m";
     public static final String RED_UNDERLINED = "\u001B[4;31m";
     public static final String GREEN_UNDERLINED = "\u001B[4;32m";
@@ -34,7 +30,6 @@ public class AnsiColors {
     public static final String CYAN_UNDERLINED = "\u001B[4;36m";
     public static final String WHITE_UNDERLINED = "\u001B[4;37m";
 
-    // Background
     public static final String BLACK_BACKGROUND = "\u001B[40m";
     public static final String RED_BACKGROUND = "\u001B[41m";
     public static final String GREEN_BACKGROUND = "\u001B[42m";
@@ -44,7 +39,6 @@ public class AnsiColors {
     public static final String CYAN_BACKGROUND = "\u001B[46m";
     public static final String WHITE_BACKGROUND = "\u001B[47m";
 
-    // High Intensity
     public static final String BLACK_BRIGHT = "\u001B[0;90m";
     public static final String RED_BRIGHT = "\u001B[0;91m";
     public static final String GREEN_BRIGHT = "\u001B[0;92m";
@@ -54,7 +48,6 @@ public class AnsiColors {
     public static final String CYAN_BRIGHT = "\u001B[0;96m";
     public static final String WHITE_BRIGHT = "\u001B[0;97m";
 
-    // Bold High Intensity
     public static final String BLACK_BOLD_BRIGHT = "\u001B[1;90m";
     public static final String RED_BOLD_BRIGHT = "\u001B[1;91m";
     public static final String GREEN_BOLD_BRIGHT = "\u001B[1;92m";
@@ -63,6 +56,5 @@ public class AnsiColors {
     public static final String PURPLE_BOLD_BRIGHT = "\u001B[1;95m";
     public static final String CYAN_BOLD_BRIGHT = "\u001B[1;96m";
     public static final String WHITE_BOLD_BRIGHT = "\u001B[1;97m";
-
 }
 
