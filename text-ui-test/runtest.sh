8c8e01f0354921717d9142806caed5257d696d0b
NicholasTanYY <nicholastanyunyu@gmail.com>
2024-03-30 11:02:57 +0800

    Add text-ui-test back

---
+++ text-ui-test/runtest.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# delete output from previous run
+if [ -e "./text-ui-test/ACTUAL-UNIX.TXT" ]
+then
+    rm ./text-ui-test/ACTUAL-UNIX.TXT
+fi
+
+# reset saved file from previous run
+if [ -e "save/tasks.txt" ]
+then 
+    rm save/tasks.txt
+fi
+
+./gradlew clean shadowJar
+
+# compile the code into the bin folder, terminates if error occurred
+if ! ./gradlew build
+then
+    echo "********** BUILD FAILURE **********"
+    exit 1
+fi
+
+# run the program, feed commands from input.txt file and redirect the output to the ACTUAL-UNIX.TXT
+./gradlew -q run < ./text-ui-test/input.txt > ./text-ui-test/ACTUAL-UNIX.TXT
+
+# compare the output to the expected output
+diff ./text-ui-test/EXPECTED-UNIX.TXT ./text-ui-test/ACTUAL-UNIX.TXT
+if [ $? -eq 0 ]
+then
+    echo "Test passed!"
+    exit 0
+else
+    echo "Test failed!"
+    exit 1
+fi
