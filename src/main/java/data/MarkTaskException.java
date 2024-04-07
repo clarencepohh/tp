d84f26a2271d767dac64babfdddb8620262d0a26
NicholasTanYY <nicholastanyunyu@gmail.com>
2024-04-07 17:45:31 +0800

    Add javadocs for new exception methods

--- src/main/java/data/MarkTaskException.java
+++ src/main/java/data/MarkTaskException.java
@@ -2,6 +2,11 @@ package data;
 
 import java.util.List;
 
+/**
+ * Represents an exception that occurs when marking a task.
+ * Inherits from TaskManagerException class.
+ * 
+ */
 public class MarkTaskException extends TaskManagerException {
 
     public static final String TASK_INDEX_OUT_OF_RANGE_FOR_DAY_WITH_TASKS_MESSAGE = 
@@ -9,10 +14,22 @@ public class MarkTaskException extends TaskManagerException {
     public static final String TASK_INDEX_WITH_NO_TASKS_MESSAGE = 
             "There are no tasks to mark on this day!";
 
+    /**
+     * Constructor for MarkTaskException class.
+     * 
+     * @param errorMessage The error message to be displayed.
+     */
     public MarkTaskException(String errorMessage) {
         super(errorMessage);
     }
 
+    /**
+     * Method that checks if the task index is valid for the given day.
+     * 
+     * @param dayTasks The list of tasks for the day.
+     * @param taskIndex The index of the task to be marked.
+     * @throws MarkTaskException if the task index is invalid.
+     */
     public static void checkIfTaskIndexIsValid(List<Task> dayTasks, int taskIndex) 
             throws MarkTaskException {
         
