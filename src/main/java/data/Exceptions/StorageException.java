aff90006b07a3da3f1e41219b31724ca1bd72fa8
kyhjonathan <jonathan.kuek01@gmail.com>
2024-04-09 16:21:12 +0800

    Create checkFileReadable method

--- src/main/java/data/Exceptions/StorageException.java
+++ src/main/java/data/Exceptions/StorageException.java
@@ -1,4 +1,15 @@
 package data.Exceptions;
 
 public class StorageException extends Exception{
+
+    public StorageException(String errorMessage) {
+        super(errorMessage);
+    }
+
+    public static void checkFileReadable(boolean canRead) throws StorageException {
+        if (!canRead) {
+            throw new StorageException("File is not readable!");
+        }
+    }
+
 }
