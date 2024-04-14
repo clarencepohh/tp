9a2407442a1e3bcae2621ad666e11196fe55b219
Aak242 <110832731+Aak242@users.noreply.github.com>
2024-04-14 21:51:51 +0800

    Fix checkstyle

--- src/main/java/data/exceptions/TaskManagerException.java
+++ src/main/java/data/exceptions/TaskManagerException.java
@@ -4,7 +4,6 @@ import data.Task;
 import time.WeekView;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.List;
