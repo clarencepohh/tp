b4c2a69edbc88d9ec11079c70ab5ef0ca858d1c0
kyhjonathan <jonathan.kuek01@gmail.com>
2024-04-14 21:33:31 +0800

    Remove unused imports from Event Class

--- src/main/java/data/Event.java
+++ src/main/java/data/Event.java
@@ -1,12 +1,5 @@
 package data;
 
-import net.fortuna.ical4j.model.DateTime;
-import net.fortuna.ical4j.model.component.VEvent;
-
-import java.util.GregorianCalendar;
-
-import static java.lang.Integer.parseInt;
-
 public class Event extends Task {
     protected String startDate;
     protected String endDate;
